1574ba6a64a8a053b4bfd2ffa30f42d6d991cf00
Jared Chan <jchan84@binghamton.edu>
2019-04-12 14:01:07 -0400

    Backend Changes

--- app.js
+++ app.js
@@ -1,46 +1,62 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var request = require("request")
 
-app.use(bodyParser.urlencoded({extended:true}))
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'));
-app.set ("view engine", "ejs");
+app.set("view engine", "ejs");
 
 var stocks = require("./request")
 
 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
   res.render("portfolio")
 })
 
 
-app.get("/trading", function(req,res){
+app.get("/trading", function (req, res) {
   res.render("trading")
 })
-var ticker = async function(symbol){
+var ticker = async function (symbol) {
   var price = await stocks.getStockData(symbol)
-  console.log(price)
   return price;
 }
-app.get("/testEndpoint", function(req, res){
+app.get("/testEndpoint", function (req, res) {
   var symbol = req.query.symbol;
   var qty = req.query.qty;
   // var stock_price = await stocks.getStockData(symbol);
   // console.log(stock_price);
-  var stock =  {
-    name: ticker(symbol),
-    // buy_price: ticker(),
-    // sell_price: ticker(askPrice)
-    number: 
-}
-  console.log(stock.name)
+
+  // let stock = ticker(symbol);
+  let stockPrice;
+  request("https://api.iextrading.com/1.0/tops?symbols=" + symbol, function(error, response, body){
+    console.log()
+    var responseFromIex = JSON.parse(body);
+    if(error){
+      res.redirect("trading")
+      console.log(error)
+    
+    } 
+   else{
+      console.log(body)
+    }
+    stockName = responseFromIex[0].symbol
+    buyPrice = responseFromIex[0].bidPrice
+    sellPrice = responseFromIex[0].askPrice
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({price: stockPrice}));
+  });
   
 })
 
-app.listen(3000,async function(){
+app.listen(3000, async function () {
   console.log("Server started on PORT 3000")
 
-  var stock_price = await stocks.getStockData("fb")
+  // var stock_price = await stocks.getStockData("fb")
   // console.log(stock_price)
 })
 
